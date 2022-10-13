82a62bdffe4779d80e79c5bbdb25cff30fdd1497
lauta <lautarolopezruiz@gmail.com>
2022-10-13 11:52:11 -0300

    add product detail

--- home/src/HomeContent.jsx
+++ home/src/HomeContent.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import {getProducts} from "./products";
+import {currency, getProducts} from "./products";
 
 
 export default function HomeContent() {
@@ -12,6 +12,17 @@ export default function HomeContent() {
     return <div className="my-10 grid grid-cols-4 gap-5">
         {products.map((product) => <div key={product.id}>
             <img src={product.image} alt={product.name}/>
+            <div className="flex">
+                <div className="flex-grow font-bold">
+                    <a>{product.name}</a>
+                </div>
+                <div className="flex-end">
+                    {currency.format(product.price)}
+                </div>
+            </div>
+            <div className="text-sm mt-4">
+                {product.description}
+            </div>
         </div>)}
     </div>
 }
