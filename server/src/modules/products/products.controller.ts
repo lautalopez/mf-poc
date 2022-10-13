07f6991161902acda9177aa0c984d59d81c37488
lauta <lautarolopezruiz@gmail.com>
2022-10-13 11:48:35 -0300

    add products list in server

--- server/src/modules/products/products.controller.ts
+++ server/src/modules/products/products.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, Param} from '@nestjs/common';
+import products, {Product} from '../../products';
 
 @Controller('products')
 export class ProductsController {
-  constructor() {}
+    constructor() {
+    }
 
-  @Get()
-  async index(): Promise<boolean> {
-    return true;
-  }
+    @Get()
+    async index(): Promise<Product[]> {
+        return products;
+    }
+
+    @Get(':id')
+    async show(@Param('id') id: string): Promise<Product> {
+        return products.find((product) => product.id === parseInt(id));
+    }
 }
